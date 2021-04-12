9fb3ebee8b8bdda495565aecb2e556bbc6055a31
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-04-12 17:09:06 +0300

    Updates

---
+++ precache-manifest.0fd48c02ec379e7580ef7b255111fd5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe22c317b1eb15d0569fcef88c4d48c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79a133feb3e00b2a05c9",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "9b326095e4a2c8c7ca63",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "79a133feb3e00b2a05c9",
+    "url": "./static/js/2.eca53828.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.eca53828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b326095e4a2c8c7ca63",
+    "url": "./static/js/main.f45ff1b4.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
